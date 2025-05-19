b579a973a3013aefd125222cdb16e1ae4e7f888c
Sam <nsamuel.gt@gmail.com>
2025-05-19 17:13:16 -0600

    Lab 03

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,38 @@
 package ed.lab;
+import java.util.*;
 
-public class E01TopKFrequentElements {
+class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+
+        for (var entry : map.entrySet()) {
+            pq.offer(entry.getKey());
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        int[] result = new int[k];
+
+        int index = 0;
+
+        while (!pq.isEmpty()) {
+            result[index] = pq.poll();
+            index++;
+
+        }
+
+        return result;
     }
 }
+
--- src/main/java/ed/lab/E02AutocompleteSystem.java
+++ src/main/java/ed/lab/E02AutocompleteSystem.java
@@ -1,13 +1,99 @@
 package ed.lab;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class E02AutocompleteSystem {
 
-    public E02AutocompleteSystem(String[] sentences, int[] times) {
+    class TrieNode implements Comparable<TrieNode> {
+        TrieNode[] children;
+        String s;
+        int times;
+        List<TrieNode> hot;
 
+        public TrieNode() {
+            children = new TrieNode[128];  // Tabla ASCII
+            s = null;
+            times = 0;
+            hot = new ArrayList<>();
+        }
+
+        @Override
+        public int compareTo(TrieNode o) {
+            if (this.times == o.times) {
+                return this.s.compareTo(o.s);
+            }
+            return o.times - this.times; // Orden descendente por frecuencia
+        }
+
+        public void update(TrieNode node) {
+            if (!this.hot.contains(node)) {
+                this.hot.add(node);
+            }
+
+            Collections.sort(hot);
+
+            if (hot.size() > 3) {
+                hot.remove(hot.size() - 1);
+            }
+        }
+    }
+
+    TrieNode root;
+    TrieNode cur;
+    StringBuilder sb;
+
+    public E02AutocompleteSystem(String[] sentences, int[] times) {
+        root = new TrieNode();
+        cur = root;
+        sb = new StringBuilder();
+
+        for (int i = 0; i < sentences.length; i++) {
+            add(sentences[i], times[i]);
+        }
+    }
+
+    public void add(String sentence, int t) {
+        TrieNode tmp = root;
+        List<TrieNode> visited = new ArrayList<>();
+
+        for (char c : sentence.toCharArray()) {
+            if (tmp.children[c - ' '] == null) {
+                tmp.children[c - ' '] = new TrieNode();
+            }
+            tmp = tmp.children[c - ' '];
+            visited.add(tmp);
+        }
+
+        tmp.s = sentence;
+        tmp.times += t;
+
+        for (TrieNode node : visited) {
+            node.update(tmp);
+        }
     }
 
     public List<String> input(char c) {
-        return null;
+        List<String> res = new ArrayList<>();
+
+        if (c == '#') {
+            add(sb.toString(), 1);
+            sb = new StringBuilder();
+            cur = root;
+            return res;
+        }
+
+        sb.append(c);
+        if (cur != null) {
+            cur = cur.children[c - ' '];
+        }
+
+        if (cur == null) return res;
+        for (TrieNode node : cur.hot) {
+            res.add(node.s);
+        }
+
+        return res;
     }
 }
